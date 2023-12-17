8ce29e27f1428120722641c3c55b083f746ebcd8
andrei <andrei.lazo.30@gmail.com>
2023-12-17 17:14:19 +0800

    image upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,14 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
-import morgan from 'morgan'
+import morgan from 'morgan';
 import authRouter from './router/authRoute.js';
 import postRoute from './router/postRoute.js';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
 
@@ -19,7 +22,7 @@ declare module 'express-session' {
     user: {
       id: number;
       username: string;
-      role: string
+      role: string;
     };
   }
 }
@@ -29,20 +32,17 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
-    saveUninitialized: true, 
+    saveUninitialized: true,
     cookie: { secure: false }, //true in prod
   })
 );
 
 app.use(cors());
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 
 //Routers
-app.use('/', authRouter)
-app.use('/', postRoute)
-
-
-
+app.use('/', authRouter);
+app.use('/', postRoute);
 
 app.listen(PORT, () => {
   console.log(`LISTENING ON PORT ${PORT}`);
--- backend/src/router/postRoute.ts
+++ backend/src/router/postRoute.ts
@@ -1,14 +1,35 @@
 import express from 'express';
-import { isAuthenticated } from '../middleware/middleware.js';
+import { isAuthenticated, validate } from '../middleware/middleware.js';
+import { db } from '../utils/db.server.js';
+import { upload } from '../utils/cloudinary.js';
+import { postSchema, postTypeSchema } from '../utils/PostSchema.js';
 
 const postRoute = express.Router();
 
+postRoute.post('/post', isAuthenticated, upload.array('image'), validate(postSchema), async (req, res) => {
+  try {
+    const data: postTypeSchema = req.body;
+    const files: Express.Multer.File[] = req.files as Express.Multer.File[];
+    const images = files?.map(file => ({ url: file.path, filename: file.filename }));
 
-postRoute.get('/authenticated', isAuthenticated, (req,res)=>{
-    res.send(`YOU ARE AUTHENTICATED AS ${req.session.user?.username}`)
-})
+    const newPost = await db.post.create({
+      data: {
+        ...data,
+        userId: req.session.user?.id!,
+        photos: {
+          create: images
+        },
+      },
+      include: {
+        photos: true,
+      },
+    });
 
+    res.status(201).json({ message: 'Successfully created new post', data: newPost });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-
-
-export default postRoute
+export default postRoute;
