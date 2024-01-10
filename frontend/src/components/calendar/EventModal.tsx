717ccbb1b1692c2318232fdd9e18e29073d92bd5
rekasa123 <regeecasana2002@gmail.com>
2024-01-10 15:00:05 +0800

    Calendaryo

--- frontend/src/components/calendar/EventModal.tsx
+++ frontend/src/components/calendar/EventModal.tsx
@@ -53,7 +53,7 @@ const EventModal: React.FC = () => {
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form className="bg-white rounded-lg shadow-2xl w-1/4" onSubmit={handleSubmit}>
         <header className="bg-gray-100 px-4 py-2 flex justify-between items-center">
-          <span className="material-icons-outlined text-gray-400">drag_handle</span>
+          <img src={"/assets/icons/drag_handle.svg"} width={20} height={20} />
           <div>
             {selectedEvent && (
               <span
@@ -70,7 +70,7 @@ const EventModal: React.FC = () => {
               </span>
             )}
             <button type="button" onClick={() => setShowEventModal(false)}>
-              <span className="material-icons-outlined text-gray-400">close</span>
+              <img src={"/assets/icons/close.svg"} width={20} height={20} />
             </button>
           </div>
         </header>
@@ -86,10 +86,10 @@ const EventModal: React.FC = () => {
               className="pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
               onChange={(e) => setTitle(e.target.value)}
             />
-            <span className="material-icons-outlined text-gray-400">schedule</span>
+            <img src={"/assets/icons/event_available.svg"} width={20} height={20} />
             <p>{daySelected ? dayjs(daySelected).format("dddd, MMMM DD") : ""}</p>
 
-            <span className="material-icons-outlined text-gray-400">segment</span>
+            <img src={"/assets/icons/segment.svg"} width={20} height={20} />
             <input
               type="text"
               name="description"
@@ -99,20 +99,8 @@ const EventModal: React.FC = () => {
               className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
               onChange={(e) => setDescription(e.target.value)}
             />
-            <span className="material-icons-outlined text-gray-400">bookmark_border</span>
-            <div className="flex gap-x-2">
-              {labelsClasses.map((lblClass, i) => (
-                <span
-                  key={i}
-                  onClick={() => setSelectedLabel(lblClass)}
-                  className={`bg-${lblClass}-500 w-6 h-6 rounded-full flex items-center justify-center cursor-pointer`}
-                >
-                  {selectedLabel === lblClass && (
-                    <span className="material-icons-outlined text-white text-sm">check</span>
-                  )}
-                </span>
-              ))}
-            </div>
+            <img src={"/assets/icons/bookmark_border.svg"} width={20} height={20} />
+            <div className="flex gap-x-2"></div>
           </div>
         </div>
         <footer className="flex justify-end border-t p-3 mt-5">
