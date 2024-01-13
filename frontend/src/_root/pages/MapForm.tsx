2bc05c54f57cd14b62cba3ce9961c7bf263c1a19
andrei <andrei.lazo.30@gmail.com>
2024-01-13 20:07:07 +0800

    map plotting

--- frontend/src/_root/pages/MapForm.tsx
+++ frontend/src/_root/pages/MapForm.tsx
@@ -1,11 +1,40 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import ReactMapGl from 'react-map-gl';
+import { useEffect, useState } from 'react';
+import ReactMapGl, { Marker } from 'react-map-gl';
+
+type Point = {
+  type: string
+  coordinates: [number, number]
+}
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiZGFkZXkiLCJhIjoiY2xyOWhjcW45MDFkZjJtbGRhM2toN2k4ZiJ9.STlq7rzxQrBIiH4BbrEvoA';
 
 const MapForm = () => {
+  const [mapData, setMapData] = useState<Point[]>([]);
+
+  useEffect(() => {
+    const fetchLocations = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/locations', {
+          credentials: 'include',
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+          setMapData(data);
+        } else {
+          console.log(data.error);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchLocations();
+  }, []);
   return (
     <ReactMapGl
       mapLib={import('mapbox-gl')}
@@ -16,7 +45,13 @@ const MapForm = () => {
       }}
       style={{ height: '100vh' }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    >
+      {mapData.map((item) => (
+        <Marker latitude={item.coordinates[1]} longitude={item.coordinates[0]}>
+          
+        </Marker>
+      ))}
+    </ReactMapGl>
   );
 };
 
