fda0800dc9b9ba22437b06d724ac60bc4cd3d8ef
dreiseu <121779773+dreiseu@users.noreply.github.com>
2024-03-25 08:41:01 +0800

    Update ArchiveDetails.tsx

--- frontend/src/components/shared/ArchiveDetails.tsx
+++ frontend/src/components/shared/ArchiveDetails.tsx
@@ -11,6 +11,9 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import Loader from "./Loader";
+import { useUserContext } from "@/context/AuthContext";
+import { Button } from "../ui/button";
 
 interface ArchiveData {
   id: number;
@@ -33,14 +36,18 @@ const ArchiveDetails: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [open, setOpen] = useState<boolean>(false);
-  const updateIndex = ({ index: current }: { index: number }) => setIndex(current);
+  const updateIndex = ({ index: current }: { index: number }) =>
+    setIndex(current);
   const [index, setIndex] = React.useState(0);
   const navigate = useNavigate();
+  const { user } = useUserContext();
 
   useEffect(() => {
     const fetchArchiveData = async () => {
       try {
-        const response = await fetch(`http://localhost:8000/archive/${province}/${id}`);
+        const response = await fetch(
+          `http://localhost:8000/archive/${province}/${id}`
+        );
         if (!response.ok) {
           throw new Error("Failed to fetch archive data");
         }
@@ -59,7 +66,21 @@ const ArchiveDetails: React.FC = () => {
   }, [id]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="w-full h-full">
+        <div className="bg-red-100 w-full p-2 flex justify-between">
+          <button
+            className="border-2 hover:border-black ease-in-out duration-300 rounded-lg p-2"
+            onClick={() => navigate(-1)}
+          >
+            Back
+          </button>
+        </div>
+        <div className="w-full m-auto p-10">
+          <Loader />
+        </div>
+      </div>
+    );
   }
 
   if (error) {
@@ -83,12 +104,22 @@ const ArchiveDetails: React.FC = () => {
         >
           Back
         </button>
+        {user.role === "ADMIN" ? (
+          <div className="px-3">
+            <Button className="w-1/2 add-archive-submit ">Edit</Button>
+            <Button className="w-1/2 add-archive-cancel ">Delete</Button>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
       <div className="p-5">
         <Table className="mb-2">
           <TableBody>
             <TableRow className="border-2 hover:border-b-black">
-              <TableCell className="font-bold w-1/5 border-2">Archive Id:</TableCell>
+              <TableCell className="font-bold w-1/5 border-2">
+                Archive Id:
+              </TableCell>
               <TableCell>{archiveData.id}</TableCell>
             </TableRow>
             <TableRow className="border-2 hover:border-b-black">
@@ -96,7 +127,9 @@ const ArchiveDetails: React.FC = () => {
               <TableCell>{archiveData.title}</TableCell>
             </TableRow>
             <TableRow className="border-2 hover:border-b-black">
-              <TableCell className="font-bold border-2">Municipality:</TableCell>
+              <TableCell className="font-bold border-2">
+                Municipality:
+              </TableCell>
               <TableCell>{archiveData.municipality}</TableCell>
             </TableRow>
             <TableRow className="border-2 hover:border-b-black">
@@ -104,7 +137,9 @@ const ArchiveDetails: React.FC = () => {
               <TableCell>{archiveData.description}</TableCell>
             </TableRow>
             <TableRow className="border-2 hover:border-b-black">
-              <TableCell className="font-bold border-2">Date Created:</TableCell>
+              <TableCell className="font-bold border-2">
+                Date Created:
+              </TableCell>
               <TableCell>{archiveData.dateCreated}</TableCell>
             </TableRow>
 
@@ -134,7 +169,10 @@ const ArchiveDetails: React.FC = () => {
                 <TableCell>
                   <div className="w-full flex flex-wrap">
                     {archiveData.files
-                      .filter((file) => file.url.endsWith(".jpg") || file.url.endsWith(".png"))
+                      .filter(
+                        (file) =>
+                          file.url.endsWith(".jpg") || file.url.endsWith(".png")
+                      )
                       .map((file, index) => (
                         <div key={index} className="w-full px-2 mb-4">
                           <Carousel className="max-w-lg mx-auto">
@@ -148,7 +186,10 @@ const ArchiveDetails: React.FC = () => {
                                         onClick={() => handleOpenLightbox()}
                                         width="300"
                                         key={index}
-                                        style={{ margin: "2px", objectFit: "cover" }}
+                                        style={{
+                                          margin: "2px",
+                                          objectFit: "cover",
+                                        }}
                                         alt=""
                                       />
                                     </CardContent>
@@ -175,7 +216,11 @@ const ArchiveDetails: React.FC = () => {
                       (file, index) =>
                         file.url.endsWith(".mp4") && (
                           <div key={index}>
-                            <video controls src={file.url} className="w-full aspect-square" />
+                            <video
+                              controls
+                              src={file.url}
+                              className="w-full aspect-square"
+                            />
                           </div>
                         )
                     )}
@@ -191,7 +236,9 @@ const ArchiveDetails: React.FC = () => {
         open={open}
         close={() => setOpen(false)}
         slides={archiveData.files
-          .filter((file) => file.url.endsWith(".jpg") || file.url.endsWith(".png"))
+          .filter(
+            (file) => file.url.endsWith(".jpg") || file.url.endsWith(".png")
+          )
           .map((file) => ({ src: file.url }))}
       />
     </div>
