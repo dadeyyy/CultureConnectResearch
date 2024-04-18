d00d73f159d26d4b0fd147b99addc6c8a9a56220
chroiel12 <57791516+chroiel12@users.noreply.github.com>
2024-04-18 23:10:26 +0800

    registration and livestream

--- frontend/src/_root/pages/LiveStream.tsx
+++ frontend/src/_root/pages/LiveStream.tsx
@@ -11,56 +11,19 @@ import {
 } from "@/components/ui/carousel";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-
-// const sources = [
-//   {
-//     title: 'I am a demon',
-//     creator: 'MrBeast',
-//     views: '110 Million',
-//     dateCreate: '24 hours ago',
-//     thumbnail:
-//       'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e0846834-6d3f-48bf-89a4-b80cd5803824/dfou32q-a8d70115-4924-4a1e-b0ec-682db24cb813.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2UwODQ2ODM0LTZkM2YtNDhiZi04OWE0LWI4MGNkNTgwMzgyNFwvZGZvdTMycS1hOGQ3MDExNS00OTI0LTRhMWUtYjBlYy02ODJkYjI0Y2I4MTMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.wAbbuTHUjT0CuD4NZL82zmOLhN3sx3nxNtb4aeXxdkk',
-//   },
-//   {
-//     title: 'Unang araw palang minahal na kita',
-//     creator: 'Gloco',
-//     views: '110',
-//     dateCreate: '4 hours ago',
-//     thumbnail:
-//       'https://scontent.fsfs1-1.fna.fbcdn.net/v/t1.6435-9/50428424_1284288608376759_5096522740211384320_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGYi7YuMZqxlazw7y6FCx-y3t5DaPKM9RTe3kNo8oz1FOVHNi3VtRAI3t_GBTCTVBI&_nc_ohc=FUdTBvo4764Ab5B1UMH&_nc_ht=scontent.fsfs1-1.fna&oh=00_AfA7lo_UYZT_-603OxKTVVjYgndLF7iD4oEcXPJFt7raXw&oe=663E56A0',
-//   },
-//   {
-//     title: 'Wow grape',
-//     creator: 'IndianNigka',
-//     views: '9',
-//     dateCreate: '4 hours ago',
-//     thumbnail: 'https://i.ytimg.com/vi/FFZSgalRSQQ/maxresdefault.jpg',
-//   },
-//   {
-//     title: 'Cheese Club',
-//     creator: 'YawningBastard',
-//     views: '69',
-//     dateCreate: '23 hours ago',
-//     thumbnail:
-//       'https://static-cse.canva.com/blob/1424409/1600w-wK95f3XNRaM.jpg',
-//   },
-//   {
-//     title: 'Spiderman ey',
-//     creator: 'ChewingGum',
-//     views: '690',
-//     dateCreate: '7 hours ago',
-//     thumbnail:
-//       'https://marketplace.canva.com/EAFW7JwIojo/2/0/1600w/canva-red-colorful-tips-youtube-thumbnail-FxVVsqyawqY.jpg',
-//   },
-//   {
-//     title: 'Eye Catching Youtube Thumbnail',
-//     creator: 'GirlFromNowhere',
-//     views: '2 days ago',
-//     dateCreate: '21 hours ago',
-//     thumbnail:
-//       'https://miro.medium.com/v2/resize:fit:680/1*n0t58ubvkW8hzqf1trUlMw.jpeg',
-//   },
-// ];
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import toast from "react-hot-toast";
 
 type streamState = {
   created: string;
@@ -74,7 +37,33 @@ type streamState = {
 }[];
 
 const LiveStream = () => {
-  const [availableLivestreams, setAvailableLivestreams] = useState<streamState>([]);
+  const [availableLivestreams, setAvailableLivestreams] = useState<streamState>(
+    []
+  );
+  const [streamKey, setStreamKey] = useState("andreiodsa");
+  const [url, setUrl] = useState("lazcikashjdkashjdsa");
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(streamKey)
+      .then(() => {
+        toast.success("Stream key copied to clipboard");
+      })
+      .catch((error) => {
+        toast.error("Failed to copy stream key to clipboard:");
+      });
+  };
+
+  const copyToClipboardU = () => {
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        toast.success("Url copied to clipboard");
+      })
+      .catch((error) => {
+        toast.error("Failed to copy Url to clipboard:");
+      });
+  };
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [error, setError] = useState<Error | null>(null);
   const navigate = useNavigate();
@@ -82,7 +71,9 @@ const LiveStream = () => {
   useEffect(() => {
     const fetchLiveStreamData = async () => {
       try {
-        const liveStreamResponse = await fetch("http://localhost:8000/getLiveStreams");
+        const liveStreamResponse = await fetch(
+          "http://localhost:8000/getLiveStreams"
+        );
 
         if (!liveStreamResponse.ok) {
           throw new Error("Failed to get livestream data!");
@@ -103,65 +94,142 @@ const LiveStream = () => {
   }, []);
 
   return (
-    <div className="  w-full flex xl:flex-row xs:flex-col">
+    <div className="w-full flex xl:flex-row xs:flex-col">
       <div className="w-full flex flex-col custom-scrollbar overflow-auto">
         <div className="bg-red-200 w-full p-3 flex flex-row justify-between text-center items-center px-5">
-          <div className="text-lg font-bold decoration-indigo-500">CultureConnect Live streams</div>
-          <Button className="bg-red-500">Go Live</Button>
-        </div>
-        <div className="w-full h-full flex justify-center">
-          <Carousel className="w-4/5 m-auto">
-            <CarouselContent className="h-full m-auto my-2">
-              {availableLivestreams.map((data, index) => (
-                <CarouselItem key={index}>
-                  <div className="">
-                    <Card
-                      className="cursor-pointer"
-                      onClick={() => {
-                        navigate(`/live-streams/1`);
-                      }}
+          <div className="text-lg font-bold decoration-indigo-500">
+            CultureConnect Live streams
+          </div>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button className="bg-red-500">Go Live</Button>
+            </SheetTrigger>
+            <SheetContent className="min-w-[720px]">
+              <SheetHeader>
+                <SheetTitle>Go Live</SheetTitle>
+                <SheetDescription>Description</SheetDescription>
+              </SheetHeader>
+              <div className="flex flex-col gap-4 py-4">
+                <div className="flex  flex-col gap-2">
+                  <Label htmlFor="title" className="text-base">
+                    Caption
+                  </Label>
+                  <Input id="name" value="Input Caption" className="w-full" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="description" className="text-base">
+                    Description (optional)
+                  </Label>
+                  <Input id="name" value="Pedro Duarte" className="w-full" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="url" className="text-base">
+                    Url
+                  </Label>
+                  <div className="flex flex-row">
+                    <Input id="url" value={url} className="w-full" readOnly />
+                    <Button
+                      onClick={copyToClipboardU}
+                      className="bg-white hover:bg-blue-200"
                     >
-                      <CardContent className="flex lg:flex-row xs:flex-col items-center justify-center rounded-xl p-0 pr-2 border-2 gap-3">
-                        <iframe
-                          className="object-cover aspect-video w-full rounded-l-xl"
-                          key={index}
-                          src={`https://customer-zo8okz8uxe6umby3.cloudflarestream.com/${data.uid}/iframe`}
-                          title="Example Stream video"
-                          frameBorder="0"
-                          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                          allowFullScreen
-                        ></iframe>
+                      <img src="/assets/icons/copy.svg" />
+                    </Button>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="streamkey" className="text-base">
+                    Streamkey
+                  </Label>
+                  <div className="flex flex-row">
+                    <Input
+                      id="streamkey"
+                      value={streamKey}
+                      className="w-full"
+                      readOnly
+                    />
+                    <Button
+                      onClick={copyToClipboard}
+                      className="bg-white hover:bg-blue-200"
+                    >
+                      <img src="/assets/icons/copy.svg" />
+                    </Button>
+                  </div>
+                </div>
+                <iframe
+                  width="100%"
+                  height="315"
+                  src="https://www.youtube.com/embed/Aa0IFks-nAE?si=Kzmxaq0PnN08Urn2"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
+              </div>
 
-                        <div className="flex flex-col w-1/2">
-                          <span className="flex flex-row text-center items-center gap-4 my-5">
-                            <img
-                              src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwiLBJBdBtEKcMeSH__f1L0CdeqX1yyYsq3uF53SLESM0_qkA7QPTCN8TtEuyIpJygRsed"
-                              width={75}
-                              className="rounded-full"
-                            />
-                            <span className="flex flex-col">
-                              <span className="text-xl font-bold text-center">Rick Astley</span>
-                              <span className="text-md text-start">@RickJackson</span>
+              <SheetFooter>
+                <SheetClose asChild>
+                  <Button type="submit" className="w-full">
+                    Go Live
+                  </Button>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
+        <Carousel className="lg:w-[720px] xl:w-[1080px] m-auto xs:w-[330px]">
+          <CarouselContent className="h-full m-auto my-2">
+            {availableLivestreams.map((data, index) => (
+              <CarouselItem key={index}>
+                <div className="">
+                  <Card
+                    className="cursor-pointer"
+                    onClick={() => {
+                      navigate(`/live-streams/1`);
+                    }}
+                  >
+                    <CardContent className="flex lg:flex-row xs:flex-col items-center justify-center rounded-xl p-0 pr-2 border-2 gap-3">
+                      <iframe
+                        className="object-cover aspect-video w-full rounded-l-xl"
+                        key={index}
+                        src={`https://customer-zo8okz8uxe6umby3.cloudflarestream.com/${data.uid}/iframe`}
+                        title="Example Stream video"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      ></iframe>
+
+                      <div className="flex flex-col w-1/2">
+                        <span className="flex flex-row text-center items-center gap-4 my-5">
+                          <img
+                            src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwiLBJBdBtEKcMeSH__f1L0CdeqX1yyYsq3uF53SLESM0_qkA7QPTCN8TtEuyIpJygRsed"
+                            width={75}
+                            className="rounded-full"
+                          />
+                          <span className="flex flex-col">
+                            <span className="text-xl font-bold text-center">
+                              Rick Astley
+                            </span>
+                            <span className="text-md text-start">
+                              @RickJackson
                             </span>
                           </span>
+                        </span>
 
-                          <span className="text-base font-base text-justify">
-                            <h1>
-                              <b>Title:{data.meta.name}</b>
-                            </h1>
-                            <p>{data.meta.description}</p>
-                          </span>
-                        </div>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
+                        <span className="text-base font-base text-justify">
+                          <h1>
+                            <b>{data.meta.name}</b>
+                          </h1>
+                        </span>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
         <div className="flex flex-col gap-2 my-5 xl:max-h-2/3 xs:max-h-96 custom-scrollbar">
           <span className="text-xl font-bold px-3 text-center sticky top-0 bg-white opacity-80">
             Past Live Streams
